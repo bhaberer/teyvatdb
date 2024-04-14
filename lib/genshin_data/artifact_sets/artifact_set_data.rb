6938f0e00334a013e462fe99617b4fb3c06a5f45
Brian Haberer <bhaberer@gmail.com>
2024-04-14 15:50:59 -0700

    Small rubocop fixes

--- lib/genshin_data/artifact_sets/artifact_set_data.rb
+++ lib/genshin_data/artifact_sets/artifact_set_data.rb
@@ -84,7 +84,7 @@ module GenshinData
             bonus: [
               "Increases Overloaded and Burning, and Burgeon DMG by 40%.",
               "Increases Vaporize and Melt DMG by 15%.",
-              "Using Elemental Skill increases the 2-Piece Set Bonus by 50% of its starting value for 10s. Max 3 stacks.",
+              "Using Elemental Skill increases the 2-Piece Set Bonus by 50% of its starting value for 10s. Max 3 stacks."
             ]
           )
         ],
